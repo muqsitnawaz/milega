64bb3c5ef3915111bd64f3e064f1fa385eb8740e
Muqsit Nawaz <muqsitnawaz@gmail.com>
2016-02-22 04:31:43 +0500

    added footer

--- index.php
+++ index.php
@@ -133,12 +133,83 @@
     <!-- section 3 -->
     <div class="row">
     	<div class="col-md-12">
-    		
+    		<br />
     	</div>
     </div>
 
     <!-- section 3: footer -->
-    <div id='footer'></div>
+    <section id="footer-section" class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="section-heading-2">
+                        <h3 class="section-title">
+                            <span>Office Address</span>
+                        </h3>
+                    </div>
+                    
+                    <div class="footer-address">
+                        <ul style="list-style-type: none;">
+                            <li class="footer-contact"><i class="fa fa-home"></i>123, Second Street name, Address</li>
+                            <li class="footer-contact"><i class="fa fa-envelope"></i><a href="#">info@domain.com</a></li>
+                            <li class="footer-contact"><i class="fa fa-phone"></i>+92 (123) 456-7890</li>
+                        </ul>
+                    </div>
+                </div><!--/.col-md-3 -->
+                
+                
+                <div class="col-md-4">
+                    <div class="section-heading-2">
+                        <h3 class="section-title">
+                            <span>Latest Tweet</span>
+                        </h3>
+                    </div>
+                    
+                    <div class="latest-tweet">
+                        <div class="media">
+                            <div class="media-left">
+                                <i class="fa fa-twitter fa-2x media-object"></i>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">About 15 days ago</h4>
+                                <p>Finally #tutsplus start a tutorial on A Beginner’s Guide to Using #joomla . Check it out here http://t.co/i6S4zeW8Z0</p>
+                            </div>
+                        </div>
+                    </div>
+                </div><!--/.col-md-3 -->
+                
+                <div class="col-md-4">
+                    <div class="section-heading-2">
+                        <h3 class="section-title">
+                            <span>Stay With us</span>
+                        </h3>
+                    </div>
+                    <div class="subscription">
+                        <p>Sign up for our newsletter and get all the latest updates. Don't worry,
+                        we don't spam.</p>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Your E-mail" id="name" required data-validation-required-message="Please enter your name.">
+                            <br/>
+                            <input type="submit" class="btn btn-primary" value="Subscribe">
+                        </div>
+                    </div>
+                </div>
+            </div><!--/.row -->
+        </div><!-- /.container -->
+    </section>
+    <!-- End Footer Section -->
+
+    <div id="copyright-section" class="copyright-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="copyright">
+                        Copyright © 2016. All Rights Reserved. Designed and Developed by Muqsit N.</a>
+                    </div>
+                </div>
+            </div><!--/.row -->
+        </div><!-- /.container -->
+    </div>
 
     <!-- javascript -->
     <script src="js/jquery-2.1.3.js"></script>

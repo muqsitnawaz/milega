33edcd5417d2a11337c6768503b7b28ddc5a976e
muqsitnawaz <muqsitnawaz@gmail.com>
2016-03-21 04:53:20 +0500

    Changed string to varchar

--- db/migrate/20160312065705_create_companies.rb
+++ db/migrate/20160312065705_create_companies.rb
@@ -2,23 +2,23 @@ class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
       t.belongs_to :user, index:true
-      t.string :cname
-      t.string :ctype
+      t.varchar :cname
+      t.varchar :ctype
 
       t.timestamps
     end
 
     create_table :products do |t|
       t.belongs_to :company, index:true
-      t.string :pname
-      t.string :pdescrip
+      t.varchar :pname
+      t.varchar :pdescrip
       t.integer :pprice
-      t.string :pcategory
-      t.string :psubcategory
+      t.varchar :pcategory
+      t.varchar :psubcategory
       t.boolean :active
-      t.string :image1
-      t.string :image2
-      t.string :image3
+      t.varchar :image1
+      t.varchar :image2
+      t.varchar :image3
 
       t.timestamps
     end
